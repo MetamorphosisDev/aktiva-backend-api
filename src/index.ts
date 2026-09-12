e8694de0cab036e97f55537e5215a8736804de3e
Jona Al Farros <124805962+MetamorphosisDev@users.noreply.github.com>
2026-09-12 07:11:41 +0700

    feat: routes main

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import postsRoutes from "./routes/posts.routes";
 import authRoutes from "./routes/auth.routes"
 import categoriesRoutes from "./routes/categories.route"
+import bookmarksRoutes from "./routes/bookmarks.routes"
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/categories", categoriesRoutes);
+app.use("/api/bookmarks", bookmarksRoutes)
 
 // Testing
 app.get("/", (_req, res) => {
